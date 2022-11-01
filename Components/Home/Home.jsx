026fabca6f566344f0b2bb4c6263da1ff37b9971
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2022-11-02 00:02:44 +0600

    update slider

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -39,11 +39,36 @@ const Home = () => {
     speed: 2000,
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <>
       <Header />
-
       <section
         className='hero-wrap'
         style={{
